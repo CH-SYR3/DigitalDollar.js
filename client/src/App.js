9ed741e72c721e7480991b8f181afe1e06905831
Camrone Hassell <Camrone.Hassell@gmail.com>
2018-11-20 14:19:23 -0800

    Changes Removed Google maps, added new material UI with tabs and side drawer 	new file:   client/src/layouts/header.js 	new file:   client/src/layouts/tabs.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,16 @@ import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-map-react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import SimpleMap from './components/SimpleMap';
+import Header from './layouts/header';
+import Tabs from './layouts/tabs';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <AppNavBar/>
-        <SimpleMap/>
+      <Header/>
+      <Tabs style={{marginBottom: '700px'}}/>
       </div>
     );
   }
